7174a4cb0dac0166d822c34ee4d39ccc2a49e0f8
yasmine-houri <yasmeanhouri@gmail.com>
2022-04-11 13:01:07 +0200

    Corrections to gsub (2)

--- Code/1_data_preprocessing.R
+++ Code/1_data_preprocessing.R
@@ -84,7 +84,7 @@ for (file in files){
 dataset_sav <- Reduce(function(...) merge(..., all=T), data_sav)
 
 # Traitements vague 1
-vague1 <- as.data.frame(data_sav[[1]])
+vague1 <- as.data.frame(data_spss[[1]])
 vague1$vague <- 1
 names(vague1)[which(names(vague1)=="AUTCHOIX")] <- "AUTCHOI"
 names(vague1)[which(names(vague1)=="MIEUPREP")] <- "MIEUPRE"
@@ -92,29 +92,30 @@ names(vague1)[which(names(vague1)=="EMPLMUNI")] <- "EMPLMUN"
 names(vague1)[which(names(vague1)=="PORTUNIF")] <- "PORTUNI"
 
 # Traitements vague 2
-vague2 <- as.data.frame(data_sav[[2]])
+vague2 <- as.data.frame(data_spss[[2]])
 vague2$vague <- 2
-names(vague2) <- gsub("B", "", names(vague2))
+names(vague2) <- gsub("^B", "", names(vague2))
 
 # Traitements vague 3
-vague3 <- as.data.frame(data_sav[[3]])
+vague3 <- as.data.frame(data_spss[[3]])
 vague3$vague <- 3
-names(vague3) <- gsub("C", "", names(vague3))
+names(vague3) <- gsub("^C", "", names(vague3))
+names(vague3)[names(vague3) == 'IDENTIT'] <- 'CIDENTIT'
 
 # Traitements vague 4
-vague4 <- as.data.frame(data_sav[[4]])
+vague4 <- as.data.frame(data_spss[[4]])
 vague4$vague <- 4
-names(vague4) <- gsub("E", "", names(vague4))
+names(vague4) <- gsub("^E", "", names(vague4))
 
 # Traitements vague 5
-vague5 <- as.data.frame(data_sav[[5]])
+vague5 <- as.data.frame(data_spss[[5]])
 vague5$vague <- 5
-names(vague5) <- gsub("F", "", names(vague5))
+names(vague5) <- gsub("^F", "", names(vague5))
 
 # Traitements vague 6
-vague6 <- as.data.frame(data_sav[[6]])
+vague6 <- as.data.frame(data_spss[[6]])
 vague6$vague <- 6
-names(vague6) <- gsub("G", "", names(vague6))
+names(vague6) <- gsub("^G", "", names(vague6))
 
 # Rbind: creation d'un dataset reunissant toutes les vagues
 df_sav <- rbind.fill(vague1,vague2,vague3,vague4,vague5,vague6)
